3bb468f62a082451e86aac4427f315ba9686f1fc
victorb-s <victorb.santos15@gmail.com>
2024-07-28 18:21:16 -0300

    Adicionar aula "Operadores"

---
+++ Modulo_01/src/edu/victor/aula_03_Operadores/OperadoresTernarios.java
@@ -0,0 +1,20 @@
+package edu.victor.aula_03_Operadores;
+
+public class OperadoresTernarios {
+    public static void main(String[] args) {
+        int a, b;
+        a = 6;
+        b = 6;
+
+        String resultado = a==b? "Verdadeiro" : "Falso";
+
+//        if(a == b){
+//            resultado = "Verdadeiro";
+//        }
+//        else{
+//            resultado = "Falso";
+//        }
+
+        System.out.println(resultado);
+    }
+}
