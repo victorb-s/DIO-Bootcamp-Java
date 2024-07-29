efa1e59ceca8ca1d447973438d55bc1d9de76f0c
victorb-s <victorb.santos15@gmail.com>
2024-07-29 00:20:35 -0300

    Adicionar aula sobre Scanners
    (cherry picked from commit 6028abeea91ded8441f2dd88c6e3cc7c6f8682ca)

---
+++ Modulo_01 - Introducao/src/edu/victor/aula_07_Scanner/SobreMim.java
@@ -0,0 +1,26 @@
+package edu.victor.aula_07_Scanner;
+
+import java.util.*;
+
+public class SobreMim {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome:");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome:");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade:");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura em metros:");
+        double altura = scanner.nextDouble();
+
+        //imprimindo os dados obtidos pelo usuario
+        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos");
+        System.out.println("Minha altura é " + altura + "m");
+    }
+}
