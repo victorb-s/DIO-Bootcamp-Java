80d8dd086db4d5f24f6f51735e2ec23bffb46d70
victorb-s <victorb.santos15@gmail.com>
2024-07-29 19:58:46 -0300

    Adicionar aula sobre Scanners

---
+++ Modulo 00 - Projetos/src/edu/victor/ContaBanco/ContaTerminal.java
@@ -0,0 +1,28 @@
+package edu.victor.ContaBanco;
+import java.util.*;
+
+/**
+ * @author Victor Barbosa
+ * @version 1.0
+ * @since 28/07/2024
+ */
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+    System.out.println("Por favor, informe o seu Nome");
+    String nomeCliente = scanner.nextLine();
+
+    System.out.println("\n" + nomeCliente + ", por favor, digite o número da Conta");
+    int numero = scanner.nextInt();
+
+    System.out.println("\n" + nomeCliente + ", por favor, digite o número da Agência");
+    int agencia = scanner.nextInt();
+
+    System.out.println("\n" + nomeCliente + ", por favor, informe o saldo disponível em sua conta");
+    double saldo = scanner.nextDouble();
+
+    System.out.printf("\nOlá %s, obrigado por criar uma conta em nosso banco, sua agência é %d, conta %d e seu saldo %2f já está disponível para saque.", nomeCliente, agencia, numero, saldo);
+    }
+}
