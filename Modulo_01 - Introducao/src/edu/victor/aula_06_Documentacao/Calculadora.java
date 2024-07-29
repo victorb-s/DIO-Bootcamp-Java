2155db9caf4472ccc5fc02b063c78b4581d76ed4
victorb-s <victorb.santos15@gmail.com>
2024-07-28 23:42:28 -0300

    Atualizar arquivo "Calculadora" da aula 06

--- Modulo_01 - Introducao/src/edu/victor/aula_06_Documentacao/Calculadora.java
+++ Modulo_01 - Introducao/src/edu/victor/aula_06_Documentacao/Calculadora.java
@@ -1,4 +1,26 @@
 package edu.victor.aula_06_Documentacao;
 
+/**
+ * <h1>Calculadora<h1/>
+ * A Calculadora realiza operações matemáticas entre números inteiros
+ * <p>
+ *     <b>Note:</b> Leia atentamente a documentação desta classe para
+ *     desfrutar dos recursos oferecidos pelo autor
+ * </p>
+ *
+ * @author Victor Barbosa
+ * @version 1.0
+ * @since 28/07/2024
+ */
+
 public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma dos dois números.
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return  numeroUm + numeroDois;
+    }
 }
