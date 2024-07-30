6d8d34693d4851c75416b7178fb71acf20f27618
victorb-s <victorb.santos15@gmail.com>
2024-07-29 23:39:12 -0300

    Atualizar aula "CondicionailTernário"

--- Modulo_02 - Fluxos/src/edu/victor/aula_04_CondicionalTernario/ResultadoEscolar.java
+++ Modulo_02 - Fluxos/src/edu/victor/aula_04_CondicionalTernario/ResultadoEscolar.java
@@ -2,9 +2,9 @@ package edu.victor.aula_04_CondicionalTernario;
 
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 7;
+        int nota = 6;
 
-        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
         System.out.println(resultado);
     }
 }
