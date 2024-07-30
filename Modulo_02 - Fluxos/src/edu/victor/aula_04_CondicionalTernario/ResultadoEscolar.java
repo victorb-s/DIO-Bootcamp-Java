1fc94ae597310fffacba97679aa0d675a3a3a738
victorb-s <victorb.santos15@gmail.com>
2024-07-29 23:36:31 -0300

    Aulas sobre fluxos condicionais

---
+++ Modulo_02 - Fluxos/src/edu/victor/aula_04_CondicionalTernario/ResultadoEscolar.java
@@ -0,0 +1,10 @@
+package edu.victor.aula_04_CondicionalTernario;
+
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 7;
+
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+    }
+}
