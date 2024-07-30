61ce18a2880f981cde9b8ca424e46e0da066e272
victorb-s <victorb.santos15@gmail.com>
2024-07-30 00:20:55 -0300

    Adicionar aula sobre "For"

---
+++ Modulo_02 - Fluxos/src/edu/victor/Fluxo02_Repeticao/aula_01_For/Contador.java
@@ -0,0 +1,11 @@
+package edu.victor.Fluxo02_Repeticao.aula_01_For;
+
+public class Contador {
+    public static void main(String[] args) {
+        for(int carneirinhos = 1; carneirinhos <= 3; carneirinhos++){
+            System.out.println("Carneirinho da vez: " + carneirinhos);
+        }
+
+        System.out.println("Vitu dormiu");
+    }
+}
