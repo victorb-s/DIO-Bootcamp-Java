84aa6e4d8767987547844257b1f830247aad8662
victorb-s <victorb.santos15@gmail.com>
2024-08-01 16:55:45 -0300

    Adicionar aula sobre "exceptions"

---
+++ Modulo_02 - Fluxos/src/edu/victor/Fluxo03_Excecoes/aula_01_TryCatch/AboutMe.java
@@ -0,0 +1,33 @@
+package edu.victor.Fluxo03_Excecoes.aula_01_TryCatch;
+
+import java.util.*;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+            System.out.println("Digite seu nome");
+            String nome = scanner.next();
+
+            System.out.println("Digite seu sobrenome");
+            String sobrenome = scanner.next();
+
+            System.out.println("Digite sua idade");
+            int idade = scanner.nextInt();
+
+            System.out.println("Digite sua altura");
+            double altura = scanner.nextDouble();
+
+
+            // Imprimindo os dados obtidos pelo usuario
+            System.out.println("Olá, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
+            System.out.println("Tenho " + idade + " anos ");
+            System.out.println("Minha altura é " + altura + "cm ");
+            scanner.close();
+        }
+        catch(InputMismatchException e) {
+            System.out.println("Os campos idade e altura precisam ser númericos");
+        }
+    }
+}
