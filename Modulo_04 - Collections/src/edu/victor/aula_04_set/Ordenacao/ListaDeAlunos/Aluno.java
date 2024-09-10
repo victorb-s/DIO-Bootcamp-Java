6200a051d2101abd9a7643b0d7e30cc1cfb73c92
victorb-s <victorb.santos15@gmail.com>
2024-09-10 19:24:33 -0300

    Adicionar atividade 'Lista de Alunos' em Set
    Signed-off-by: victorb-s <victorb.santos15@gmail.com>

---
+++ Modulo_04 - Collections/src/edu/victor/aula_04_set/Ordenacao/ListaDeAlunos/Aluno.java
@@ -0,0 +1,63 @@
+package edu.victor.aula_04_set.Ordenacao.ListaDeAlunos;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Aluno implements Comparable<Aluno>{
+    private String nome;
+    private long matricula;
+    private double media;
+
+    public Aluno(String nome, long matricula, double media) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.media = media;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Long getMatricula() {
+        return matricula;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Aluno aluno = (Aluno) o;
+        return Objects.equals(getMatricula(), aluno.getMatricula());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getMatricula());
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "nome='" + nome + '\'' +
+                ", matricula=" + matricula +
+                ", media=" + media +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Aluno a) {
+        return nome.compareToIgnoreCase(a.getNome());
+    }
+}
+
+class CompararPorNota implements Comparator<Aluno> {
+
+    @Override
+    public int compare(Aluno a1, Aluno a2) {
+        return Double.compare(a1.getMedia(), a2.getMedia());
+    }
+}
--- Modulo_04 - Collections/src/edu/victor/aula_04_set/Ordenacao/ListaDeAlunos/GerenciadorAlunos.java
+++ Modulo_04 - Collections/src/edu/victor/aula_04_set/Ordenacao/ListaDeAlunos/GerenciadorAlunos.java
@@ -1,4 +1,73 @@
 package edu.victor.aula_04_set.Ordenacao.ListaDeAlunos;
 
+import java.util.*;
+
 public class GerenciadorAlunos {
+    private Set<Aluno> gerenciadorSet;
+
+    public GerenciadorAlunos() {
+        this.gerenciadorSet = new HashSet<>();
+    }
+
+    public void adicionarAluno(String nome, long matricula, double media) {
+        gerenciadorSet.add(new Aluno(nome, matricula, media));
+    }
+
+    public void removerAluno(long matricula){
+        Aluno alunoParaRemover = null;
+        if(!gerenciadorSet.isEmpty()){
+            for(Aluno aluno : gerenciadorSet){
+                if(aluno.getMatricula() == matricula){
+                    alunoParaRemover = aluno;
+                    gerenciadorSet.remove(alunoParaRemover);
+                    break;
+                }
+            }
+        } else {
+            throw new RuntimeException("Esse aluno não está na lista");
+        }
+    }
+
+    public Set<Aluno> exibirAlunosPorNome() {
+        Set<Aluno> alunosPorNome = new TreeSet<>(gerenciadorSet);
+        if(!gerenciadorSet.isEmpty()){
+            return alunosPorNome;
+        } else {
+            throw new RuntimeException("Nenhum aluno foi encontrado");
+        }
+    }
+
+    public Set<Aluno> exibirAlunosPorNota(){
+        Set<Aluno> alunosPorNota = new TreeSet<>(new CompararPorNota());
+        alunosPorNota.addAll(gerenciadorSet);
+        if(!gerenciadorSet.isEmpty()){
+            return alunosPorNota;
+        } else {
+            throw new RuntimeException("Nenhum aluno foi encontrado");
+        }
+    }
+
+    public void exibirAlunos(){
+        if(!gerenciadorSet.isEmpty()){
+            System.out.println(gerenciadorSet);
+        } else {
+            throw new RuntimeException("Nenhum aluno foi encontrado");
+        }
+    }
+
+    public static void main(String[] args) {
+        GerenciadorAlunos gerenciadorAlunos = new GerenciadorAlunos();
+
+        gerenciadorAlunos.adicionarAluno("Aluno 1", 123L, 5);
+        gerenciadorAlunos.adicionarAluno("Aluno 2", 456L, 9.5);
+        gerenciadorAlunos.adicionarAluno("Aluno 3", 123L, 10);
+        gerenciadorAlunos.adicionarAluno("Aluno 0", 789L, 9);
+        gerenciadorAlunos.adicionarAluno("Aluno 10", 321L, 3);
+        gerenciadorAlunos.adicionarAluno("Aluno 12", 654L, 10);
+
+        gerenciadorAlunos.exibirAlunos();
+        gerenciadorAlunos.removerAluno(456L);
+        System.out.println(gerenciadorAlunos.exibirAlunosPorNome());
+        System.out.println(gerenciadorAlunos.exibirAlunosPorNota());
+    }
 }
