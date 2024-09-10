5c52486fce2fb558f1d1e2a26a826083d9437704
victorb-s <victorb.santos15@gmail.com>
2024-09-10 13:31:47 -0300

    Adicionar aula sobre 'Ordenação' em Hash
    Signed-off-by: victorb-s <victorb.santos15@gmail.com>

---
+++ Modulo_04 - Collections/src/edu/victor/aula_04_set/Ordenacao/CadastroProdutos.java
@@ -0,0 +1,48 @@
+package edu.victor.aula_04_set.Ordenacao;
+
+import java.util.*;
+
+public class CadastroProdutos {
+    private Set<Produto> produtoSet;
+
+    public CadastroProdutos() {
+        this.produtoSet = new HashSet<>();
+    }
+
+    public void adicionarProduto(long codigo, String nome, double preco, int quantidade) {
+        produtoSet.add(new Produto(codigo, nome, preco, quantidade));
+    }
+
+    public Set<Produto> exibirProdutosPorNome() {
+        Set<Produto> produtosPorNome = new TreeSet<>(produtoSet);
+        if (!produtosPorNome.isEmpty()) {
+            return produtosPorNome;
+        } else {
+            throw new RuntimeException("Lista vazia!");
+        }
+    }
+
+    public Set<Produto> exibirProdutosPorPreco(){
+        Set<Produto> produtosPorPreco = new TreeSet<>(new ComparatorPorPreco());
+        produtosPorPreco.addAll(produtoSet);
+
+        if (!produtosPorPreco.isEmpty()) {
+            return produtosPorPreco;
+        } else {
+            throw new RuntimeException("Lista vazia!");
+        }
+    }
+
+    public static void main(String[] args) {
+        CadastroProdutos cadastroProdutos = new CadastroProdutos();
+        cadastroProdutos.adicionarProduto(1L, "Produto 5", 15d, 5);
+        cadastroProdutos.adicionarProduto(2L, "Produto 0", 20d, 10);
+        cadastroProdutos.adicionarProduto(1L, "Produto 3", 10d, 2);
+        cadastroProdutos.adicionarProduto(9L, "Produto 9", 2d, 2);
+
+        System.out.println(cadastroProdutos.produtoSet);
+
+        System.out.println(cadastroProdutos.exibirProdutosPorNome());
+        System.out.println(cadastroProdutos.exibirProdutosPorPreco());
+    }
+}
