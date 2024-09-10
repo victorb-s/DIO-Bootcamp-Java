fa25e22d2ace1fb37257cda2b9b806fe84be9f6c
victorb-s <victorb.santos15@gmail.com>
2024-09-10 12:30:53 -0300

    Criar aula sobre 'Operacoes Basicas em Set'
    Signed-off-by: victorb-s <victorb.santos15@gmail.com>

---
+++ Modulo_04 - Collections/src/edu/victor/aula_04_set/OperacoesBasicas/ConjuntoConvidados.java
@@ -0,0 +1,56 @@
+package edu.victor.aula_04_set.OperacoesBasicas;
+
+import java.util.*;
+
+public class ConjuntoConvidados {
+    private Set<Convidado> convidadoSet;
+
+    public ConjuntoConvidados() {
+        this.convidadoSet = new HashSet<>();
+    }
+
+    public void adicionarConvidado(String nome, int codigoConvite){
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+
+    public void removerConvidado(int codigoConvite){
+        Convidado convidadoParaRemover = null;
+        for(Convidado convidado : convidadoSet){
+            if(convidado.getCodigoConvite() == codigoConvite){
+                convidadoParaRemover = convidado;
+                break;
+            }
+        }
+
+        if(convidadoParaRemover != null){
+            convidadoSet.remove(convidadoParaRemover);
+        } else {
+            throw new RuntimeException("Não existe convidado com o codigo de convite: " + codigoConvite);
+        }
+    }
+
+    public int contarConvidados(){
+        return convidadoSet.size();
+    }
+
+    public void exibirConvidados(){
+        System.out.println(convidadoSet);
+    }
+
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidados dentro do Set de convidados");
+
+        conjuntoConvidados.adicionarConvidado("Convidado 1", 1234);
+        conjuntoConvidados.adicionarConvidado("Convidado 2", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidado 3", 1235); // Não foi salvo, pois no hash não permite elementos iguais (codigoConvite == id)
+        conjuntoConvidados.adicionarConvidado("Convidado 4", 1236);
+
+        conjuntoConvidados.exibirConvidados();
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidados dentro do Set de convidados");
+
+        conjuntoConvidados.removerConvidado(1234);
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidados dentro do Set de convidados");
+    }
+}
