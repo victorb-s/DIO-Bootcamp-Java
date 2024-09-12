d5a28edceea7cb1948cce0f657ec0e9ba71e06e1
victorb-s <victorb.santos15@gmail.com>
2024-09-12 19:29:46 -0300

    Testar o projeto da aula 'Pesquisa'
    Signed-off-by: victorb-s <victorb.santos15@gmail.com>

--- Modulo_04 - Collections/src/edu/victor/aula_05_map/Pesquisa/EstoqueDeProdutos/EstoqueProdutos.java
+++ Modulo_04 - Collections/src/edu/victor/aula_05_map/Pesquisa/EstoqueDeProdutos/EstoqueProdutos.java
@@ -78,6 +78,17 @@ public class EstoqueProdutos {
     }
 
     public static void main(String[] args) {
+        EstoqueProdutos estoque = new EstoqueProdutos();
+        estoque.exibirProdutos();
 
+        estoque.adicionarProduto(1L, "Produto A", 10, 5.0);
+        estoque.adicionarProduto(2L, "Produto B", 5, 10.0);
+        estoque.adicionarProduto(3L, "Produto C", 2, 15.0);
+
+        estoque.exibirProdutos();
+
+        System.out.println("Valor total do estoque: R$" + estoque.calcularValorTotalEstoque());
+        System.out.println("Produto mais barato: " + estoque.obterProdutoMaisBarato());
+        System.out.println("Produto mais caro: " + estoque.obterProdutoMaisCaro());
     }
 }
