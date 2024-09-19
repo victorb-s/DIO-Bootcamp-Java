c8f61037eec9de85cdd6bec15ea303510e711cc4
victorb-s <victorb.santos15@gmail.com>
2024-09-19 15:10:27 -0300

    Adicionar aula sobre 'Ordernação' em Map
    Signed-off-by: victorb-s <victorb.santos15@gmail.com>

---
+++ Modulo_04 - Collections/src/edu/victor/aula_05_map/Ordenacao/AgendaDeEventos/AgendaEventos.java
@@ -0,0 +1,47 @@
+package edu.victor.aula_05_map.Ordenacao.AgendaDeEventos;
+
+import java.time.*;
+import java.util.*;
+
+public class AgendaEventos {
+    private Map<LocalDate, Evento> eventosMap;
+
+    public AgendaEventos(){
+        this.eventosMap = new HashMap<>();
+    }
+
+    public void adicionarEvento(LocalDate data, String nome, String atracao){
+        eventosMap.put(data, new Evento(nome, atracao));
+    }
+
+    public void exibirAgenda(){
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+        System.out.println(eventosTreeMap);
+    }
+
+    public void obterProximoEvento(){
+        LocalDate dataAtual = LocalDate.now();
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+        for(Map.Entry<LocalDate, Evento> entry : eventosTreeMap.entrySet()){
+            // proximaData = getKey
+            // proximoEvento = getValue
+            if(entry.getKey().isEqual(dataAtual) || entry.getKey().isAfter(dataAtual)){
+                System.out.println("O próximo evento: " + entry.getValue() + " acontecerá na data " + entry.getKey());
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        AgendaEventos agendaEventos = new AgendaEventos();
+        agendaEventos.adicionarEvento(LocalDate.of(2022, Month.JULY, 9), "Evento 1", "Atração 1");
+        agendaEventos.adicionarEvento(LocalDate.of(2022, 7, 15), "Evento 2", "Atração 2");
+        agendaEventos.adicionarEvento(LocalDate.of(2000, Month.JANUARY, 10), "Evento 3", "Atração 3");
+        agendaEventos.adicionarEvento(LocalDate.of(2024, Month.SEPTEMBER, 30), "Evento 4", "Atração 4");
+        agendaEventos.adicionarEvento(LocalDate.of(2024, Month.SEPTEMBER, 19), "Evento 5", "Atração 5");
+
+        agendaEventos.exibirAgenda();
+
+        agendaEventos.obterProximoEvento();
+    }
+}
